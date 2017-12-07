05d7d04464056cccb1ce882147dbfa4295b840bc
John Wojciechowski <jwojo13@gmail.com>
2017-12-07 15:18:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(array)
+    word_letters = @word.split("").sort
+    array.select {|x|x.split("").sort
+    word_letters == array_letters
+    end
+  end
+
+
+end
